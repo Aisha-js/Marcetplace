0ec193790a1afa251a51b3d856520081357186a4
Aisha-js <93536134+Aisha-js@users.noreply.github.com>
2022-01-17 13:07:44 +0600

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,28 +10,10 @@ const HomePage = () => {
       <Carousel.Item interval={1000}>
         <img className="d-block w-100" src={Img1} alt="First slide" />
       </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img className="d-block w-100" src={Img2} alt="Second slide" />
-        <img className="d-block w-100" src={Img3} alt="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src="" alt="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-
         <CarouselSecondSlide />
-
       </Carousel.Item>
     </Carousel>
-
     </Container>
   );
 };
